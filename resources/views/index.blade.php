e5fed8389615f8657c24779a8e3f50fadd48cd7f
tw1997 <t888y@qq.com>
2017-07-08 22:11:48 +0800

    登陆页面

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
-    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0,minimum-scale=1.0,user-scalable=no" />
     <title>新生报到</title>
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
     
