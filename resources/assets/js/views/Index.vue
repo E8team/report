2bcc55eeb1bffe38d8345a6ae27f9de5caf870b2
tw1997 <t888y@qq.com>
2017-07-08 15:33:17 +0800

    添加vux

---
+++ resources/assets/js/views/Index.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1>这是Index页面</h1>
+</template>
+
+
+<script>
+    export default{
+        name: 'index',
+        components: {
+        },
+        mounted () {
+        }
+    }
+</script>
+
+<style lang="less" scoped>
+</style>
