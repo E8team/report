0d6468b4177cc273c5ec55a07b52c7bf300bc779
taoyu <1255803549@qq.com>
2017-07-09 19:21:51 +0800

    fix

--- app/Http/Controllers/Api/StudentsController.php
+++ app/Http/Controllers/Api/StudentsController.php
@@ -11,7 +11,7 @@ class StudentsController extends ApiController
 {
     public function searchStudents($partOfStudentName, StudentRepositoryInterface $userRepository)
     {
-        $studentNames = $userRepository->searchUsers($partOfStudentName, 10, ['student_name']);
+        $studentNames = $userRepository->searchStudents($partOfStudentName, 10, ['student_name']);
         if(!$studentNames->isEmpty()){
             $studentNames = $studentNames->pluck('student_name');
         }
@@ -30,5 +30,10 @@ class StudentsController extends ApiController
     public function me()
     {
         return $this->response->item(Auth::user(), new StudentTransformer());
+    }
+
+    public function setReport()
+    {
+        $student = Auth::user();
     }
 }
